c5e65c6238d47d85ca3ac92cec230376bda1ffd9
abdulwahab009900 <152057096+abdulwahab009900@users.noreply.github.com>
2024-05-20 21:58:38 +0500

    Update config.js

--- config.js
+++ config.js
@@ -26,8 +26,8 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 //💌------------------------------------------💌
 
 
-//💌global.pairingNumber = "" //put your bot number here💌
-global.mods = ['923042205427'] 
+//💌global.pairingNumber = "923285715826" //put your bot number here💌
+global.mods = ['923285715826'] 
 global.prems = ['']
 global.allowed = ['']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
